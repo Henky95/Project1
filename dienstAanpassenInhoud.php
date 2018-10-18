a34651420908e3ebdc025d5f6da58bf4fdef4bdc
Jente Boonstra <44258633+JenteB@users.noreply.github.com>
2018-10-18 14:37:51 +0200

    Delete dienstAanpassenInhoud.php

--- dienstAanpassenInhoud.php
+++
@@ -1,7 +0,0 @@
-<form>
-    Titel: <br>
-    <input type="text" name="naam"><br>
-    Omschrijving: <br>
-    <textarea rows="5" cols="40" name="beschrijving"></textarea><br>
-    <input type="submit" value="Opslaan"><br>
-</form>
