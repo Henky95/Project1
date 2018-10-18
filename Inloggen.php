9742237ebce81e5e37230f7feb91d7d1c4b52dfa
Jente Boonstra <44258633+JenteB@users.noreply.github.com>
2018-10-18 14:38:09 +0200

    Delete Inloggen.php

--- Inloggen.php
+++
@@ -1,7 +0,0 @@
-<form action="naInloggen.php" method="post">
-    Accountnaam: <br>
-    <input type="text" name="accountnaam"><br>
-    Wachtwoord: <br>
-    <input type="password" name="ww"><br>
-    <input type="submit" value="Inloggen"><br>
-</form>
