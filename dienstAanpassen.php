468ab53889a3d8baaea9a8e28c1bac7b737b9557
Jente Boonstra <44258633+JenteB@users.noreply.github.com>
2018-10-18 14:37:43 +0200

    Delete dienstAanpassen.php

--- dienstAanpassen.php
+++
@@ -1,8 +0,0 @@
-<form action="dienstAanpassenInhoud.php" method="post">
-    Selecteer een dienst: <br>
-    <!--foreach loop voor diensten van het account, allemaal onder elkaar weergeven
-        Ook dan het met naam en dergelijke weer laten geven-->
-    <input type="submit" value="dienst1"><br>
-    <input type="submit" value="dienst2"><br>
-    <input type="submit" value="dienst3"><br>
-</form>
