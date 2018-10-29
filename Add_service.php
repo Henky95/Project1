1ac238f6ba9626abf2150c1cc2af57e17cc83a8d
Henky95 <Henky95@users.noreply.github.com>
2018-10-29 13:49:54 +0100

    Add a way to add a service

---
+++ Add_service.php
@@ -0,0 +1,51 @@
+<?php include "shared/Header.php"; ?>
+
+    <h3 class="header">
+        Voeg een dienst Toe
+    </h3>
+
+    <form method="post">
+        <div class="center">
+            <label for="title">Titel: </label>
+            <input name="title" id="title" type="text"/>
+        </div>
+        <br/>
+        <div class="center">
+            <label for="Description">Omschrijving</label>
+            <br/>
+            <textarea name="Description" id="Description" rows="10" cols="100"></textarea>
+        </div>
+        <div class="center">
+            <label for="ReturnService">retourdienst</label>
+            <br/>
+            <textarea name="ReturnService" id="ReturnService" rows="10"
+                      cols="100">&euro; 10,- s'il vous pla&icirc;t</textarea>
+        </div>
+        <input type="submit" value="Aanbieden">
+    </form>
+
+<?php
+include 'functions.php';
+
+if (!empty($_POST)) {
+    if (isset($_POST['title']) && !empty($_POST['title']) &&
+        isset($_POST['Description']) && !empty($_POST['Description']) &&
+        isset($_POST['ReturnService']) && !empty($_POST['ReturnService'])) {
+
+        $title = test_input($_POST['title']);
+        $Description = test_input($_POST['Description']);
+        $ReturnService = test_input($_POST['ReturnService']);
+
+        $query = "insert into mydb.services (services.Title, services.Description, services.ReturnService, services.IsRequest, services.Users_Id)
+                     value ('$title','$Description','$ReturnService', false, 1)";
+
+//        echo $query;
+
+        echo Query($query);
+    } else {
+        echo 'vul alles in';
+    }
+}
+?>
+
+<?php include "Shared/Footer.html"; ?>
