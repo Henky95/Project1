d2f0957f1974304188158c5c3f01beb0adbbad60
rizza99 <44263376+rizza99@users.noreply.github.com>
2018-11-08 13:35:29 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -13,6 +13,31 @@ function test_input($data)
     return $data;
 }
 
+Function LoggedIn(){
+	return $_SESSION["Ingelogd"] == true;
+}
+
+function GetCurrrentUserName(){
+	$id = GetCurrentUserId();
+	if ($id != null){
+		Query("select FirstName from mydb.users where Id = $id")
+		
+		
+		
+		return 		
+	}	else
+	{
+		return null;
+	}
+}
+
+function GetCurrentUserId(){
+	if (LoggedIn()){
+	return $_SESSION["accountID"];
+	}else{
+		return null;
+	}
+}
 
 Function Query($query){
     $db = new mysqli("localhost","root" , "");
