46ada2fc6ffd8453a5450fe75653a3ad889fa602
Jente Boonstra <44258633+JenteB@users.noreply.github.com>
2018-11-08 13:41:05 +0100

    Update checkInloggen.php

--- checkInloggen.php
+++ checkInloggen.php
@@ -1,28 +1,31 @@
 <?php
 include "functions.php";
-include "dienstDB.php";
-include "Header.php";
+include "database.php";
+include "shared/Header.php";
 
 if(isset($_POST["accountnaam"], $_POST["wachtwoord"])) {
 	$nameEmail = test_input($_POST["accountnaam"]);
-	$nameEmail = mysqli_real_escape_string($db, $nameEmail);
+	$nameEmail = mysqli_real_escape_string($dbConnection, $nameEmail);
 	$password = test_input($_POST["wachtwoord"]);
-	$password = mysqli_real_escape_string($db, $password);
+	$password = mysqli_real_escape_string($dbConnection, $password);
 	
 	$query = "SELECT EmailAdress, Password FROM Users WHERE EmailAdress ='".$nameEmail."' AND Password = '".$password."'";
-	$result1 = mysqli_query($db,$query);
+	$result1 = mysqli_query($dbConnection,$query);
 	
 	if(mysqli_num_rows($result1) > 0) {
 		$_SESSION["Ingelogd"] = true;
 		$_SESSION["naamEmail"] = $nameEmail;
 		$queryAccountID = "SELECT id FROM Users WHERE EmailAdress='".$nameEmail."'";
-		$_SESSION["accountID"] = mysqli_query($db,$queryAccountID);
+		$_SESSION["accountID"] = mysqli_query($dbConnection,$queryAccountID);
 		echo "Login succesvol, welkom!<br>";
 	} else {
 		echo "De gebruikersnaam of wachtwoord is onjuist!<br>";
 	}
 }
 
-include "shared/Footer.php";
 ?>
 <a href = "Inloggen.php">Naar inlogpagina</a>
+
+<?php 
+include "shared/Footer.html";
+?>
