9d79c2a683f6a5c6174a98c9bbf6908447e5e28f
rizza99 <44263376+rizza99@users.noreply.github.com>
2018-11-12 17:53:52 +0100

    aangepast css

--- sendemail.php
+++ sendemail.php
@@ -1,4 +1,5 @@
 <?php include 'Shared/header.php';?>
+
 <?php
     $msg = "";
 	use PHPMailer\PHPMailer\PHPMailer;
@@ -35,31 +36,17 @@
 			echo $mail->ErrorInfo;
 	}
 ?>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Contact Form</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-</head>
-<body>
-	<div class="container" style="margin-top: 100px">
-		<div class="row justify-content-center">
-			<div class="col-md-6 col-md-offset-3" align="center">
 
+	<div class="centered"
+<br>
                 <?php if ($msg != "") echo "$msg<br><br>"; ?>
 
 				<form method="post" action="sendemail.php" enctype="multipart/form-data">
-					<input class="form-control" name="subject" placeholder="Subject..."><br>
-					<input class="form-control" name="email" type="email" placeholder="Email..."><br>
-					<textarea placeholder="Message..." class="form-control" name="message"></textarea><br>
-					<input class="btn btn-primary" name="submit" type="submit" value="Send Email">
+					<input class="nice" name="subject" placeholder="Subject..."><br><br>
+					<input class="nice" name="email" type="email" placeholder="Email..."><br><br>
+					<textarea placeholder="Message..." class="form-control" name="message"></textarea><br><br>
+					<input class="centered" name="submit" type="submit" value="Send Email">
 				</form>
-			</div>
-		</div>
 	</div>
 
 <?php include 'Shared/Footer.html';?>
